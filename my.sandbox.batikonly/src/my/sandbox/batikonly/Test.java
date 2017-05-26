6a007132b84949a7e8347a643c8c13852aaaedb2
Stefan Bischof <Stefan.Bischof@jena.de>
2017-05-26 15:23:44 +0200

    batik and servicemix only ;runsystemcapabilities

---
+++ my.sandbox.batikonly/src/my/sandbox/batikonly/Test.java
@@ -0,0 +1,39 @@
+package my.sandbox.batikonly;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.bridge.UserAgent;
+import org.apache.batik.bridge.UserAgentAdapter;
+import org.apache.batik.dom.svg.SVGDocumentFactory;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.w3c.dom.svg.SVGDocument;
+
+@Component(service = Object.class, immediate = true)
+public class Test {
+
+	byte[] svg = "<svg height=\"100\" width=\"100\"><circle cx=\"50\" cy=\"50\" r=\"40\" fill=\"red\" /></svg>"
+			.getBytes();
+
+
+
+	@Activate
+	public void runBatik() {
+		System.out.println("Start");
+
+		UserAgent userAgent = new UserAgentAdapter();
+		SVGDocumentFactory documentFactory = new SAXSVGDocumentFactory(userAgent.getXMLParserClassName(), true);
+		documentFactory.setValidating(userAgent.isXMLParserValidating());
+
+		try {
+			SVGDocument document = documentFactory.createSVGDocument(null, new ByteArrayInputStream(svg));
+			System.out.println(document.getInputEncoding());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("end");
+	}
+}
